009d3eecb46c382126470b04a4ec2bdf383c81d4
lailsonzw <franciscolailsondealmeida0p9@gmail.com>
2024-01-16 11:08:23 -0300

    questão 2 esta quase pronta

---
+++ tipos estruturados/output/questao2.c
@@ -0,0 +1,106 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct person
+{
+    char name [20];
+    int documents;
+    int age;
+}data;//Estrutura criada para armazenar os dados de uma pessoa;
+
+void def_quantidade(int *quantidade)//Funcao criada para definir a quantidade de pessoas no progama;
+{
+    printf("Quantas pessoas vao participar?\n");
+    scanf("%d",&*quantidade);
+}
+
+void preencher(data *user)//Funcao criada para preencher os dados das pessoas;
+{
+    printf("Digite o seu nome:\n");
+    scanf("%s",user->name);
+    printf("Digite o numero do seu documento:\n");
+    scanf("%d",&user->documents);
+    printf("Digite sua idade:\n");
+    scanf("%d",&user->age);
+    system("clear");
+}
+
+void mostrar(data *user)//Funcao para mostrar os dados dos usuarios;
+{
+    printf("DADOS\n");
+    printf("Nome: %s\n",user->name);
+    printf("Idade: %d\n",user->age);
+    printf("Numero do documento: %d\n\n", user->documents);
+}
+
+void newage(data *user)
+{
+    int years;
+    printf("Faz quantos anos essa pessoa nao atualizou sua idade:\n");
+    scanf("%d",&years);
+    
+    printf("Idade atualizada\n");
+    printf("Idade antiga: %d\n", user->age);
+    user->age = user->age + years;
+    printf("Idade atual: %d\n",user->age);
+    
+}
+
+int main()
+{
+    int quantidade; //Variavel criada para definir a quantidade de pessoas no progama;
+    def_quantidade(&quantidade);
+
+    //Questão 2, letra A - META, montar uma funcao que preencha os dados dos usuarios - COMPLETA;
+    
+    data person[quantidade];//Variavel do tipo data adicionada no progama;
+    for(int ordem = 0; ordem < quantidade; ordem++)
+    {
+        preencher(&person[ordem]);
+    }
+
+    //Questão 2, letra B - META, montar uma funcao que imprima os dados de cada pessoa - COMPLETA;
+
+    for(int ordem = 0; ordem < quantidade; ordem++)
+    {
+        printf("Usuario %d\n", ordem + 1);
+        mostrar(&person[ordem]);
+    }
+
+    //Questão 2, letra C - META, fazer uma funcao que atualize a idade dos users - COMPLETA;
+    
+    int escolha = 0;//variavel criada para o usuario fazer a escolha abaixo;
+    printf("Alguem precisa atualizar a idade?\n1-sim 2-nao\n");
+    scanf("%d",&escolha);
+    system("clear");
+
+    if(escolha == 1)
+    {
+        for(int ordem = 0; ordem < quantidade; ordem++)
+        {
+            printf("Usuario %d\n", ordem + 1);
+            mostrar(&person[ordem]);
+        }
+        
+        int documento = 0;
+        printf("Digite o numero do documento da pessoa que voce deseja alterar:\n");
+        scanf("%d",&documento);
+        system("clear");
+
+        for(int ordem = 0; ordem < quantidade; ordem++)
+        {
+            if(person[ordem].documents == documento)
+            {
+                newage(&person[ordem]);
+                sleep(5);
+            }
+        }
+
+    }
+
+    //Questão 2, letra D - META, fazer uma funcao que mostre a pessoa mais velha e a pessoa mais nova - EM PROCESSO;
+
+    
+}
